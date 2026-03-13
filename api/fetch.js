147130a749c255804f9aca275e524a3a6f01b2ce
ColdGolem <deeperseekerz@gmail.com>
2026-03-13 13:44:40 +0530

    Update fetch.js

--- api/fetch.js
+++ api/fetch.js
@@ -1,1 +1,14 @@
 
+let messagesDB = {
+  blue: [],
+  red: [],
+  green: []
+};
+
+export default function handler(req, res) {
+  const line = req.query.line;
+  if (!line) return res.status(400).json({ error: 'Missing line parameter' });
+
+  const messages = messagesDB[line] || [];
+  res.status(200).json({ messages });
+}
