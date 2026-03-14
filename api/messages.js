cb340bcbbb702b120dcc62ee9f7784197b9bdebf
Nuan <265183555+nuan-0@users.noreply.github.com>
2026-03-14 15:10:28 +0530

    Update messages.js

--- api/messages.js
+++ api/messages.js
@@ -1,1 +1,37 @@
+let messages = {};
 
+export default function handler(req, res) {
+
+  if (req.method === "POST") {
+
+    const { city, line, text } = req.body;
+
+    const room = `${city}-${line}`;
+
+    if (!messages[room]) {
+      messages[room] = [];
+    }
+
+    messages[room].push({
+      text,
+      time: Date.now()
+    });
+
+    // keep last 50 messages only
+    messages[room] = messages[room].slice(-50);
+
+    res.status(200).json({ success: true });
+
+  }
+
+  else if (req.method === "GET") {
+
+    const { city, line } = req.query;
+
+    const room = `${city}-${line}`;
+
+    res.status(200).json(messages[room] || []);
+
+  }
+
+}
